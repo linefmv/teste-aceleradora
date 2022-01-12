2335119e37e7d8fc5fb356ad33754ab57539e5c1
Aline Viana <alinemaciel018@gmail.com>
2022-01-12 14:46:04 -0300

    feat: select inputs and create dinamic table

---
+++ challenge/index.js
@@ -0,0 +1,25 @@
+function addRow() {
+    const tableBody = document.getElementById("table-body");
+    let nameOfStudent = document.createElement("th");
+    let notesFirstSemester = document.createElement("th");
+    let notesSecondSemester = document.createElement("th");  
+    let frequency = document.createElement("th");    
+    
+    const row = document.createElement("tr");
+    
+    nameOfStudent.innerHTML = document.getElementById("studentName").value;
+    notesFirstSemester.innerHTML  = document.getElementById("firstSemesterNotes").value;
+    notesSecondSemester.innerHTML  = document.getElementById("secondSemesterNotes").value;
+    frequency.innerHTML  = document.getElementById("frequency").value;
+    
+row.appendChild(nameOfStudent);
+row.appendChild(notesFirstSemester);
+row.appendChild(notesSecondSemester);
+row.appendChild(frequency);
+
+
+tableBody.appendChild(row);
+
+}
+
+
