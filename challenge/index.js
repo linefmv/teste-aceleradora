e9a83b767bce525a7a33ce18ad3536f0fd65f1bf
Aline Viana <alinemaciel018@gmail.com>
2022-01-14 19:59:50 -0300

    feat: add funcionality of send forms

--- challenge/index.js
+++ challenge/index.js
@@ -1,38 +1,63 @@
+
+const reprove = 0;
+const aprove = 1;
+const setReprove = () => (row.style.backgroundColor = "red");
+
+const sendForms = document.getElementById("add");
+const table = document.getElementById("table");
+
 function addRow() {
-    const tableBody = document.getElementById("table-body");
-    let nameOfStudent = document.createElement("th");
-    let notesFirstSemester = document.createElement("th");
-    let notesSecondSemester = document.createElement("th");  
-    let frequency = document.createElement("th");    
-    
-    const row = document.createElement("tr");
-    
-    nameOfStudent.innerHTML = document.getElementById("studentName").value;
-    notesFirstSemester.innerHTML  = document.getElementById("firstSemesterNotes").value;
-    notesSecondSemester.innerHTML  = document.getElementById("secondSemesterNotes").value;
-    frequency.innerHTML  = document.getElementById("frequency").value;
-    
-row.appendChild(nameOfStudent);
-row.appendChild(notesFirstSemester);
-row.appendChild(notesSecondSemester);
-row.appendChild(frequency);
+  const tableBody = document.getElementById("table-body");
+  let nameOfStudent = document.createElement("td");
+  let notesFirstSemester = document.createElement("td");
+  let notesSecondSemester = document.createElement("td");
+  let frequency = document.createElement("td");
+  
+  const row = document.createElement("tr");
 
+  const studentNameValue = document.getElementById("studentName").value;
+  const firstSemesterValue = document.getElementById("firstSemesterNotes").value;
+  const secondSemesterValue = document.getElementById("secondSemesterNotes").value;
+  const frequencyValue = document.getElementById("frequency").value
 
-tableBody.appendChild(row);
-
+  
+  nameOfStudent.innerHTML = studentNameValue 
+  notesFirstSemester.innerHTML = firstSemesterValue
+  notesSecondSemester.innerHTML = secondSemesterValue
+  frequency.innerHTML = frequencyValue
+  
+  row.appendChild(nameOfStudent);
+  row.appendChild(notesFirstSemester);
+  row.appendChild(notesSecondSemester);
+  row.appendChild(frequency);
+  
+  if (allAnswers < 5) {
+    const setReprove = () => (document.getElementByTagName("tr").style.color = "red");
+      return setReprove()
+  } 
+  tableBody.appendChild(row);
+  
 }
 
-const sendInput = document.getElementById('add')
-const table = document.getElementById('table')
-const showTable = () => table.style.display = "block";
-sendInput.addEventListener('click', () => {
-    showTable()
-})
+
+const answers = document.querySelectorAll("td");
+
+const allAnswers = answers.forEach(element => {
+  console.log(element)
+});
+
+
+const showTable = () => (table.style.display = "block");
+sendForms.addEventListener("click", () => {
+  showTable();
+});
+
+const keepGoingMessage = () => confirm("Você deseja enviar?")
 
 function buttonAddFunctions() {
-    addRow();
-    showTable();
+  addRow();
+  showTable();
+  keepGoingMessage()
 }
-
 
 
