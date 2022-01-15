913070886950beb47eb6b38e801d9ebfed4d8df2
Aline Viana <alinemaciel018@gmail.com>
2022-01-14 22:15:56 -0300

    refactor: set input values to number and add float

--- challenge/index.js
+++ challenge/index.js
@@ -1,11 +1,4 @@
 
-const reprove = 0;
-const aprove = 1;
-const setReprove = () => (row.style.backgroundColor = "red");
-
-const sendForms = document.getElementById("add");
-const table = document.getElementById("table");
-
 function addRow() {
   const tableBody = document.getElementById("table-body");
   let nameOfStudent = document.createElement("td");
@@ -15,49 +8,39 @@ function addRow() {
   
   const row = document.createElement("tr");
 
-  const studentNameValue = document.getElementById("studentName").value;
-  const firstSemesterValue = document.getElementById("firstSemesterNotes").value;
-  const secondSemesterValue = document.getElementById("secondSemesterNotes").value;
-  const frequencyValue = document.getElementById("frequency").value
+  const studentNameValue =  document.getElementById("studentName").value;
+  const firstSemesterValue = parseFloat(document.getElementById("firstSemesterNotes").value);
+  const secondSemesterValue = parseFloat(document.getElementById("secondSemesterNotes").value);
+  const frequencyValue = document.getElementById("frequency").value;
 
-  
-  nameOfStudent.innerHTML = studentNameValue 
-  notesFirstSemester.innerHTML = firstSemesterValue
-  notesSecondSemester.innerHTML = secondSemesterValue
-  frequency.innerHTML = frequencyValue
-  
+  nameOfStudent.innerHTML = studentNameValue
+  notesFirstSemester.innerHTML = firstSemesterValue.toFixed(2);
+  notesSecondSemester.innerHTML = secondSemesterValue.toFixed(2)
+  frequency.innerHTML = frequencyValue + '%'
+
   row.appendChild(nameOfStudent);
   row.appendChild(notesFirstSemester);
   row.appendChild(notesSecondSemester);
   row.appendChild(frequency);
   
-  if (allAnswers < 5) {
-    const setReprove = () => (document.getElementByTagName("tr").style.color = "red");
-      return setReprove()
-  } 
   tableBody.appendChild(row);
   
 }
 
-
-const answers = document.querySelectorAll("td");
-
-const allAnswers = answers.forEach(element => {
-  console.log(element)
-});
-
+const sendForms = document.getElementById("add");
+const table = document.getElementById("table");
 
 const showTable = () => (table.style.display = "block");
 sendForms.addEventListener("click", () => {
   showTable();
 });
 
-const keepGoingMessage = () => confirm("Você deseja enviar?")
+const confirmSendForm = () => confirm("Você deseja enviar?")
 
 function buttonAddFunctions() {
   addRow();
   showTable();
-  keepGoingMessage()
+  confirmSendForm()
 }
 
 
