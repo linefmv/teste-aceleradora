c5195c16c06261c604ae7da64045545313cf6250
Aline Viana <alinemaciel018@gmail.com>
2022-01-14 23:51:55 -0300

    refactor: improve code and format

--- challenge/index.js
+++ challenge/index.js
@@ -1,34 +1,35 @@
-
 function addRow() {
   const tableBody = document.getElementById("table-body");
   let nameOfStudent = document.createElement("td");
   let notesFirstSemester = document.createElement("td");
   let notesSecondSemester = document.createElement("td");
   let frequency = document.createElement("td");
-  
+
   const row = document.createElement("tr");
 
   const studentNameValue = document.getElementById("studentName").value;
-  const firstSemesterValue = parseFloat(document.getElementById("firstSemesterNotes").value);
-  const secondSemesterValue = parseFloat(document.getElementById("secondSemesterNotes").value);
+  const firstSemesterValue = parseFloat(
+    document.getElementById("firstSemesterNotes").value
+  );
+  const secondSemesterValue = parseFloat(
+    document.getElementById("secondSemesterNotes").value
+  );
   const frequencyValue = Number(document.getElementById("frequency").value);
 
-  nameOfStudent.innerHTML = studentNameValue
+  nameOfStudent.innerHTML = studentNameValue;
   notesFirstSemester.innerHTML = firstSemesterValue.toFixed(2);
-  notesSecondSemester.innerHTML = secondSemesterValue.toFixed(2)
-  frequency.innerHTML = frequencyValue + '%'
-  
-  
+  notesSecondSemester.innerHTML = secondSemesterValue.toFixed(2);
+  frequency.innerHTML = frequencyValue + "%";
+
   row.appendChild(nameOfStudent);
   row.appendChild(notesFirstSemester);
   row.appendChild(notesSecondSemester);
   row.appendChild(frequency);
-  
+
   tableBody.appendChild(row);
 
   const allAnswers = document.querySelectorAll("td");
-  allAnswers.forEach(answer => {   console.log(answer);
-    answer.style.backgroundColor = "green" });
+  allAnswers.forEach((answer) => (answer.style.backgroundColor = "green"));
 }
 
 const sendForms = document.getElementById("add");
@@ -39,12 +40,10 @@ sendForms.addEventListener("click", () => {
   showTable();
 });
 
-const confirmSendForm = () => confirm("Você deseja enviar?")
+const confirmSendForm = () => confirm("Você deseja enviar?");
 
 function buttonAddFunctions() {
   addRow();
   showTable();
-  confirmSendForm()
+  confirmSendForm();
 }
-
-
